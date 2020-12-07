c3622a2d61b9d24c2300e38c5b79183472145755
Douglas Hirsh <douglas@codeup.com>
2020-12-07 11:11:06 -0600

    feat: Implement security

---
+++ src/main/java/com/codeup/blog/controllers/UserController.java
@@ -0,0 +1,35 @@
+package com.codeup.blog.controllers;
+
+import com.codeup.blog.models.User;
+import com.codeup.blog.repos.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UserController {
+    private UserRepository usersDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository usersDao, PasswordEncoder passwordEncoder) {
+        this.usersDao = usersDao;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @GetMapping("/sign-up")
+    public String showSignupForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        usersDao.save(user);
+        return "redirect:/login";
+    }
+}
--- src/main/java/com/codeup/blog/repos/UserRepository.java
+++ src/main/java/com/codeup/blog/repos/UserRepository.java
@@ -4,4 +4,5 @@ import com.codeup.blog.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
